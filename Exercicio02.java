7e13d0a5e85797b61b96545e62af8e5122c6917e
Marcio <marciopedro15@gmail.com>
2018-05-10 10:24:55 -0300

    Exercicio concluido

--- Exercicio02.java
+++ Exercicio02.java
@@ -8,11 +8,14 @@ public class Exercicio02 {
 		for(int i = 0;i != idades.length;i++){
 			idades[i]=Integer.parseInt(JOptionPane.showInputDialog(null,
 					"Informe a " + (i+1) + "ª idade a ser gravado.",
-					"Exerciocio Vetor com For 02",JOptionPane.PLAIN_MESSAGE));
+					"Exercicio Vetor com For 02",JOptionPane.PLAIN_MESSAGE));
+		}
+		for(int i = 0;i != idades.length;i++){
 			idade += idades[i] +"\n";
 		}
 		JOptionPane.showMessageDialog(null,idade,"Idades Informadas",JOptionPane.PLAIN_MESSAGE);
-	
+		
 	}
-
+	
 }
+			
